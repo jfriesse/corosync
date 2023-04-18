jfriesse/corosync

commit 155c0d40521313adc0c452e19d711dde54eb16b0

totemudpu: Scale receive buffer
Receive buffer should be based on PROCESSOR_COUNT_MAX and not static
buffer.

Signed-off-by: Jan Friesse <jfriesse@redhat.com>
Reviewed-by: Christine Caulfield <ccaulfie@redhat.com>

exec/totemudpu.c
@@ -74,7 +74,14 @@
 #define MSG_NOSIGNAL 0
 #endif
 
-#define UDPU_FRAME_SIZE_MAX 10000
+/*
+ * Estimation of required buffer size - it should be at least
+ *   sizeof(memb_join) + PROCESSOR_MAX * 2 * sizeof(srp_addr))
+ * if we want to support PROCESSOR_MAX nodes, but because we don't have
+ * srp_addr and memb_join, we have to use estimation.
+ * TODO: Consider moving srp_addr/memb_join into totem headers instead of totemsrp.c
+ */
+#define UDPU_FRAME_SIZE_MAX	(PROCESSOR_COUNT_MAX * (INTERFACE_MAX * 2 * sizeof(struct totem_ip_address)) + 1024)
 
 #define MCAST_SOCKET_BUFFER_SIZE (TRANSMITS_ALLOWED * UDPU_FRAME_SIZE_MAX)
 #define NETIF_STATE_REPORT_UP		1
@@ -407,6 +414,7 @@ static int net_deliver_fn (
 	struct iovec *iovec;
 	struct sockaddr_storage system_from;
 	int bytes_received;
+	int truncated_packet;
 
 	iovec = &instance->totemudpu_iov_recv;
 
@@ -438,6 +446,31 @@ static int net_deliver_fn (
 		return (0);
 	} else {
 		instance->stats_recv += bytes_received;
+	}
+
+	truncated_packet = 0;
+
+#ifdef HAVE_MSGHDR_FLAGS
+	if (msg_recv.msg_flags & MSG_TRUNC) {
+		truncated_packet = 1;
+	}
+#else
+	/*
+	 * We don't have MSGHDR_FLAGS, but we can (hopefully) safely make assumption that
+	 * if bytes_received == UDPU_FRAME_SIZE_MAX then packet is truncated
+	 */
+	if (bytes_received == UDPU_FRAME_SIZE_MAX) {
+		truncated_packet = 1;
+	}
+#endif
+
+	if (truncated_packet) {
+		log_printf (instance->totemudpu_log_level_error,
+				"Received too big message. This may be because something bad is happening"
+				"on the network (attack?), or you tried join more nodes than corosync is"
+				"compiled with (%u) or bug in the code (bad estimation of "
+				"the UDPU_FRAME_SIZE_MAX). Dropping packet.", PROCESSOR_COUNT_MAX);
+		return (0);
 	}
 
 	iovec->iov_len = bytes_received;

commit c691044fc4e64ef8a8a0bdfaa0a6da38c81055b1

DO NOT MERGE - add buffer cache
UDPU performance lost in whitetank branch compared to needle is for UDPU
caused by 155c0d40521313adc0c452e19d711dde54eb16b0 which increases udpu
buffer. Increased buffer makes malloc slower making quite a big
performance hit.

Solution for UDPU is to add cache which will store buffers in the stack
so they are not allocated/freed over and over again.

This helps UDPU to get to similar CPU usage as needle.

It doesn't help KNET at all.

This patch may help some other scenario, like cpgbench so it is worth to
revisit later.

It is required to make gc (garbage collect) more clever (probably call
it once X seconds and store minimum stack usage when calling
allocate/release).

Signed-off-by: Jan Friesse <jfriesse@redhat.com>

exec/Makefile.am
@@ -36,7 +36,8 @@ noinst_HEADERS		= apidef.h cs_queue.h logconfig.h main.h \
 			  totemnet.h totemudp.h \
 			  totemudpu.h totemsrp.h util.h vsf.h \
 			  schedwrk.h sync.h fsm.h votequorum.h vsf_ykd.h \
-			  totemknet.h stats.h ipcs_stats.h
+			  totemknet.h stats.h ipcs_stats.h \
+			  totembcache.h
 
 sbin_PROGRAMS		= corosync
 
@@ -47,7 +48,8 @@ corosync_SOURCES	= vsf_ykd.c coroparse.c vsf_quorum.c sync.c \
 			  ipc_glue.c service.c logconfig.c totemconfig.c \
 			  totemip.c totemnet.c totemudp.c \
 			  totemudpu.c totemsrp.c \
-			  totempg.c totemknet.c
+			  totempg.c totemknet.c \
+			  totembcache.c
 
 if BUILD_MONITORING
 corosync_SOURCES	+= mon.c

exec/totembcache.c
@@ -1,0 +1,106 @@
+/*
+ * Copyright (c) 2023 Red Hat, Inc.
+ *
+ * All rights reserved.
+ *
+ * Author: Jan Friesse (jfriesse@redhat.com)
+ *
+ * This software licensed under BSD license, the text of which follows:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the MontaVista Software, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "totembcache.h"
+
+void totembcache_initialize(struct totembcache *bcache,
+    void *(*buffer_alloc)(void),
+    void (*buffer_release)(void *ptr))
+{
+
+	memset(bcache, 0, sizeof(*bcache));
+
+	bcache->buffer_alloc = buffer_alloc;
+	bcache->buffer_release = buffer_release;
+
+	bcache->stack_top = NULL;
+}
+
+void *totembcache_buffer_alloc(struct totembcache *bcache)
+{
+	void *res;
+	void *prev;
+
+	res = NULL;
+
+	if (bcache->stack_top != NULL) {
+		res = bcache->stack_top;
+		prev = *((void **)bcache->stack_top);
+
+		bcache->stack_top = prev;
+	} else {
+		res = bcache->buffer_alloc();
+	}
+
+	return (res);
+}
+
+void totembcache_buffer_release(struct totembcache *bcache, void *ptr)
+{
+
+	*((void **)ptr) = bcache->stack_top;
+	bcache->stack_top = ptr;
+}
+
+void totembcache_finalize(struct totembcache *bcache)
+{
+
+	void *prev;
+
+	while (bcache->stack_top != NULL) {
+		prev = *((void **)bcache->stack_top);
+
+		bcache->buffer_release(bcache->stack_top);
+
+		bcache->stack_top = prev;
+	}
+}
+
+void totembcache_gc(struct totembcache *bcache)
+{
+	static int counter = 0;
+
+	void *prev;
+
+	if (((counter++) % 10 == 0) && bcache->stack_top != NULL) {
+		prev = *((void **)bcache->stack_top);
+
+		bcache->buffer_release(bcache->stack_top);
+
+		bcache->stack_top = prev;
+	}
+}

exec/totembcache.h
@@ -1,0 +1,57 @@
+/*
+ * Copyright (c) 2023 Red Hat, Inc.
+ *
+ * All rights reserved.
+ *
+ * Author: Jan Friesse (jfriesse@redhat.com)
+ *
+ * This software licensed under BSD license, the text of which follows:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the MontaVista Software, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef TOTEMBCACHE_H_DEFINED
+#define TOTEMBCACHE_H_DEFINED
+
+struct totembcache {
+	void *(*buffer_alloc)(void);
+
+	void (*buffer_release)(void *ptr);
+
+	void *stack_top;
+};
+
+extern void totembcache_initialize(struct totembcache *bcache,
+    void *(*buffer_alloc)(void),
+    void (*buffer_release)(void *ptr));
+
+extern void *totembcache_buffer_alloc(struct totembcache *bcache);
+
+extern void totembcache_buffer_release(struct totembcache *bcache, void *ptr);
+
+extern void totembcache_finalize(struct totembcache *bcache);
+
+extern void totembcache_gc(struct totembcache *bcache);
+
+#endif /* TOTEMBCACHE_H_DEFINED */

exec/totemnet.c
@@ -43,6 +43,8 @@
 #include <totemnet.h>
 #include <qb/qbloop.h>
 
+#include "totembcache.h"
+
 #define LOGSYS_UTILS_ONLY 1
 #include <corosync/logsys.h>
 
@@ -260,6 +262,8 @@ struct totemnet_instance {
                 ...)__attribute__((format(printf, 6, 7)));
 
         int totemnet_subsys_id;
+
+        struct totembcache buffer_cache;
 };
 
 #define log_printf(level, format, args...)				\
@@ -287,6 +291,10 @@ static void totemnet_instance_initialize (
 		"Initializing transport (%s).", transport_entries[transport].name);
 
 	instance->transport = &transport_entries[transport];
+
+	totembcache_initialize(&instance->buffer_cache,
+	    instance->transport->buffer_alloc,
+	    instance->transport->buffer_release);
 }
 
 int totemnet_crypto_set (
@@ -308,6 +316,8 @@ int totemnet_finalize (
 {
 	struct totemnet_instance *instance = (struct totemnet_instance *)net_context;
 	int res = 0;
+
+	totembcache_finalize(&instance->buffer_cache);
 
 	res = instance->transport->finalize (instance->transport_context);
 
@@ -367,17 +377,18 @@ error_destroy:
 void *totemnet_buffer_alloc (void *net_context)
 {
 	struct totemnet_instance *instance = net_context;
+
 	assert (instance != NULL);
-	assert (instance->transport != NULL);
-	return instance->transport->buffer_alloc();
+
+	return (totembcache_buffer_alloc(&instance->buffer_cache));
 }
 
 void totemnet_buffer_release (void *net_context, void *ptr)
 {
 	struct totemnet_instance *instance = net_context;
 	assert (instance != NULL);
-	assert (instance->transport != NULL);
-	instance->transport->buffer_release (ptr);
+
+	totembcache_buffer_release(&instance->buffer_cache, ptr);
 }
 
 int totemnet_processor_count_set (
@@ -418,6 +429,8 @@ int totemnet_token_send (
 {
 	struct totemnet_instance *instance = (struct totemnet_instance *)net_context;
 	int res = 0;
+
+	totembcache_gc(&instance->buffer_cache);
 
 	res = instance->transport->token_send (instance->transport_context, msg, msg_len);
 

exec/totemudpu.c
@@ -1048,11 +1048,13 @@ int totemudpu_initialize (
 
 void *totemudpu_buffer_alloc (void)
 {
+//	printf("alloc\n");
 	return malloc (FRAME_SIZE_MAX);
 }
 
 void totemudpu_buffer_release (void *ptr)
 {
+//	printf("free\n");
 	return free (ptr);
 }
 

test/cpgverify.c
@@ -151,7 +151,7 @@ int main (int argc, char *argv[])
 	 */
 	i = 0;
 	do {
-		msg.msg_size = 100 + rand() % 100000;
+		msg.msg_size = 50000;
 		iov[1].iov_len = msg.msg_size;
 		for (j = 0; j < msg.msg_size; j++) {
 			buffer[j] = j;
@@ -175,6 +175,7 @@ try_again_one:
 			exit(1);
 		}
 		i++;
+		poll(NULL, 0, 10);
 	} while (run_forever || i < iter);
 
 	cpg_finalize (handle);